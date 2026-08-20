90c42d137ab1a004d4afb15bc415b3d52258c59a
Balde <pups9827@gmail.com>
2026-08-20 23:23:38 +0300

    Add files via upload

---
+++ bullet.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """Класс для упрвлением снарядами, выпущеным кораблем. """
+
+    def __init__(self, ai_game):
+        """Создает объект снарядов в текущей позиции корабля. """
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # создание снаряда в позиции(0, 0) и назначение правильной позиции.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+            self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # позиция снаряда хранится в вещесвенном формате.
+        self.y = float(self.rect.y)
--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         # Насройки коробля
         self.ship_speed = 1.5
+
+        # параметры снаряда
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
